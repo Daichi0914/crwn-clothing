b381ca5aa46271a7ba4fa3d7fcdd776af24612e2
Daichi0914 <goludrozya44@gmail.com>
2020-07-07 03:17:47 +0900

    implenented firebese utils, included ability to store authenticated users into our firstore database

---
+++ src/test.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firebase.collection('user').doc('GMNTt2cJ2Z9fJYxgkbLo').collection('cartItems').doc('6Do6zhXXWdSR564oNorQ');
+firebase.doc('/users/GMNTt2cJ2Z9fJYxgkbLo/cartItems/6Do6zhXXWdSR564oNorQ');
+firebase.collection('users/GMNTt2cJ2Z9fJYxgkbLo/cartItems');
